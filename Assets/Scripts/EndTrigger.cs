87c8b1b320418da82a82a8c52ec66b3f224b0db1
Antaine <aconn@outlook.com>
2020-04-10 22:41:08 +0100

    Level Swap

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Level 2");
+        SceneManager.LoadScene("Level2");
+    }
+
+    private void OnTriggerEnter()
+    {
+        LoadMenu();
+    }
+}
